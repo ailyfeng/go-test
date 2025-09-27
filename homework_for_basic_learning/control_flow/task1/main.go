48e9d700da530a29fb5fab29209858d08a6217f0
kenn <2467185260@qq.com>
2025-09-27 23:24:18 +0800

    task1

--- homework_for_basic_learning/control_flow/task1/main.go
+++ homework_for_basic_learning/control_flow/task1/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func findOnlyOne(nums []int) []int{
+func findOnlyMutl(nums []int) []int{
 	
 	m:=make(map[int]int)
 	for _, v := range nums {
@@ -20,6 +20,32 @@ func findOnlyOne(nums []int) []int{
 	return res
 }
 
+func findOnlyOne2(nums []int) int{ 
+ 	ans := 0
+    for _,num:= range nums {
+        ans =ans^num
+    }
+    return ans
+}
+
+func findOnlyOne(nums []int) (int, bool){
+	
+	var res int
+	if(len(nums)==0){
+		return res,false
+	}
+	m:=make(map[int]int)
+	for _, v := range nums {
+		m[v]++
+	}
+	for k,v:=range m{
+		if v==1{
+			return k,true
+		}
+	}
+	return res,false
+}
+
 func main(){
 
 	/*
@@ -28,8 +54,8 @@ func main(){
 	找出那个只出现了一次的元素。可以使用 for 循环遍历数组，结合 if 条件判断和 map 数据结构来解决，
 	例如通过 map 记录每个元素出现的次数，然后再遍历 map 找到出现次数为1的元素。
 	*/
-	arr := []int{1,3,3,2,4,5,1}
-	m:=findOnlyOne(arr)
+	arr := []int{2,9,2,3,3}
+	m:=findOnlyOne2(arr)
 	fmt.Println(m)
-
+ 
 }
